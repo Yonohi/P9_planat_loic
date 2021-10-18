81de1b4b5ca1aaa0d979f3dc81c2eac0396ad69a
Yonohi <loic.planat2@gmail.com>
2021-10-18 14:19:52 +0200

    preview image finie

--- LITReview/static/book_review/js/preview_image.js
+++ LITReview/static/book_review/js/preview_image.js
@@ -1,3 +1,12 @@
-$("p").on("click", function(){
-  alert("The paragraph was clicked.");
+var $inputImage = $('.custom-file-input')
+var $preview = $('.img-preview')
+var preview = document.getElementsByClassName('img-preview')[0]
+$inputImage.on('change', function(e){
+    var file = event.target.files[0];
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        $preview.attr('src', e.target.result)
+    };
+    reader.readAsDataURL(file);
+    preview.style.display = 'block';
 });
